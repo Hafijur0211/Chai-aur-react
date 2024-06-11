8abb68db7970ea4752b01a9111e695a4db249ab1
Hafijur0211 <hafijur021998@gmail.com>
2024-06-11 13:17:19 +0530

    username

--- 04-tailwindprops/src/App.jsx
+++ 04-tailwindprops/src/App.jsx
@@ -21,8 +21,8 @@ function App() {
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind Test
       </h1>
-      <Card username="Chaiaurcode" />
-      <Card />
+      <Card username="Hafijur" />
+      <Card username="Rahman" />
     </>
   );
 }
