c5ba47e9f953a47bc1c1d2502f34123ebf85f187
Hafijur0211 <hafijur021998@gmail.com>
2024-06-20 11:10:39 +0530

    reactRouter

--- 08-reactRouter/src/components/Footer/Footer.jsx
+++ 08-reactRouter/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/hafijur0211"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -77,9 +77,9 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
-            © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            © 2024
+            <a href="https://hafijurrahman.com/" className="hover:underline">
+              hafijurrahman
             </a>
             . All Rights Reserved.
           </span>
