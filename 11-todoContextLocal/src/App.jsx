34c0d7fa1d4e585acf4c771ee422e19fe856b64a
Hafijur0211 <hafijur021998@gmail.com>
2024-06-29 14:44:29 +0530

    updatetodo

--- 11-todoContextLocal/src/App.jsx
+++ 11-todoContextLocal/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { TodoProvider } from "./context";
 
 const App = () => {
-  return ( 
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+  const [todos, setTodos] = useState([]);
 
-export default App
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+};
+
+export default App;
